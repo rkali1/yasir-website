607354c0874342d3f1a3c5d69cbc5c5828d78448
Raja Ali <32766998+rkali1@users.noreply.github.com>
2021-01-18 15:16:27 +0500

    Add files via upload

---
+++ action.php
@@ -0,0 +1,29 @@
+<?php
+if(isset($_POST['submit']))
+{
+
+    $name = $_POST["cf-name"];
+    
+	$email = $_POST["cf-email"];
+	
+	$content = $_POST["cf-message"];
+	
+    $to = "rk_ali@outlook.com";
+    
+    $email_subject = "Contact form submission: $name";
+    
+    $email_body = "You have received a new message. ".
+    
+    " Here are the details:\n Name: $name \n ".
+    
+    "Email: $email\n Message \n $content";
+    
+    
+    mail($to,$email_subject,$email_body);
+    
+    //redirect to the 'thank you' page
+    
+    echo "<script>location='index.html'</script>";
+   
+}
+?>
